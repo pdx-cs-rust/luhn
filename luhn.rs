3e4479b7de4ba91f1f8c507542cf75d5c0aa667f
Bart Massey <bart@cs.pdx.edu>
2023-10-19 21:14:54 -0700

    removed allocation, cleaned up errors

--- luhn.rs
+++ luhn.rs
@@ -41,7 +41,7 @@ fn luhn_sum(cc_number: &str) -> Result<(usize, [u32; 2]), LuhnError> {
 /// over the alphabet of ASCII digits and spaces. Returns
 /// `false` if the check digit is wrong or if the input is
 /// not valid.
-pub fn luhn(cc_number: &str) -> Result<bool, LuhnError> {
+pub fn luhn_check(cc_number: &str) -> Result<bool, LuhnError> {
     let (ndigits, sums) = luhn_sum(cc_number)?;
     if ndigits < 2 {
         return Err(LuhnError::Short(ndigits));
@@ -52,37 +52,37 @@ pub fn luhn(cc_number: &str) -> Result<bool, LuhnError> {
 
 #[test]
 fn test_non_digit_cc_number() {
-    assert!(matches!(luhn("foo"), Err(LuhnError::NonDigit(0, 'f'))));
-    assert!(matches!(luhn("0 foo 0"), Err(LuhnError::NonDigit(2, 'f'))));
+    assert!(matches!(luhn_check("foo"), Err(LuhnError::NonDigit(0, 'f'))));
+    assert!(matches!(luhn_check("0 foo 0"), Err(LuhnError::NonDigit(2, 'f'))));
 }
 
 #[test]
 fn test_empty_cc_number() {
-    assert!(matches!(luhn(""), Err(LuhnError::Short(0))));
-    assert!(matches!(luhn(" "), Err(LuhnError::Short(0))));
-    assert!(matches!(luhn("  "), Err(LuhnError::Short(0))));
+    assert!(matches!(luhn_check(""), Err(LuhnError::Short(0))));
+    assert!(matches!(luhn_check(" "), Err(LuhnError::Short(0))));
+    assert!(matches!(luhn_check("  "), Err(LuhnError::Short(0))));
 }
 
 #[test]
 fn test_single_digit_cc_number() {
-    assert!(matches!(luhn("0"), Err(LuhnError::Short(1))));
+    assert!(matches!(luhn_check("0"), Err(LuhnError::Short(1))));
 }
 
 #[test]
 fn test_two_digit_cc_number() {
-    assert!(luhn(" 0 0 ").unwrap());
+    assert!(luhn_check(" 0 0 ").unwrap());
 }
 
 #[test]
 fn test_valid_cc_number() {
-    assert!(luhn("4263 9826 4026 9299").unwrap());
-    assert!(luhn("4539 3195 0343 6467").unwrap());
-    assert!(luhn("7992 7398 713").unwrap());
+    assert!(luhn_check("4263 9826 4026 9299").unwrap());
+    assert!(luhn_check("4539 3195 0343 6467").unwrap());
+    assert!(luhn_check("7992 7398 713").unwrap());
 }
 
 #[test]
 fn test_invalid_cc_number() {
-    assert!(!luhn("4223 9826 4026 9299").unwrap());
-    assert!(!luhn("4539 3195 0343 6476").unwrap());
-    assert!(!luhn("8273 1232 7352 0569").unwrap());
+    assert!(!luhn_check("4223 9826 4026 9299").unwrap());
+    assert!(!luhn_check("4539 3195 0343 6476").unwrap());
+    assert!(!luhn_check("8273 1232 7352 0569").unwrap());
 }
